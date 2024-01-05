5ae54727c15a34e4ce761760dbd2f55b661a9986
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-01-05 17:54:32 -0300

    refactor: initial blockchain router and params established

--- server/routes/Blockchain.js
+++ server/routes/Blockchain.js
@@ -3,22 +3,29 @@ const axios = require("axios");
 const router = express.Router();
 
 router.get("/blockchain", async (req, res) => {
+  const search = req.query.term;
+
   try {
-    const response = await axios.get(
-      "https://bscscan.com/searchHandler?term=trx&filterby=0"
-    );
+    const response = await axios.get("https://bscscan.com/searchHandler", {
+      params: {
+        term: search,
+        filterby: 0,
+      },
+    });
+
     const responseData = response.data;
 
     res.send(
       responseData.map((data) => {
-        return { 
-					title: data.title, 
-					address: data.address,
-				};
+        return {
+          title: data.title,
+          address: data.address,
+        };
       })
     );
   } catch (error) {
-    console.log(error);
+    console.error("Erro ao buscar dados da blockchain:", error);
+    res.status(500).send("Erro ao buscar dados da blockchain");
   }
 });
 
