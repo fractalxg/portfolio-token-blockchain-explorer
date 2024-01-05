cbeef62c2ed68e054b2fef5bdb5c503047c0ddff
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-01-05 17:19:48 -0300

    refactor: server routes

--- server/routes/Blockchain.js
+++ server/routes/Blockchain.js
@@ -1,8 +1,25 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
 
-router.get("/blockchain", (req, res) => {
-    res.send({data:"teste"})
-})
+router.get("/blockchain", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://bscscan.com/searchHandler?term=trx&filterby=0"
+    );
+    const responseData = response.data;
 
-module.exports = router
+    res.send(
+      responseData.map((data) => {
+        return { 
+					title: data.title, 
+					address: data.address,
+				};
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
