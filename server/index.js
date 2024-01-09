e0a0559cfd5d8f63aa7eb04f2a9009f93d6c1b23
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-01-09 19:19:58 -0300

    feat: binance api source code on server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const server = express();
-const { port } = require("./config")
+const { port } = require("./config");
 
 server.use(
   cors({
@@ -16,9 +16,11 @@ server.use("/", binanceRoute);
 const ethereumRoute = require("./routes/Ethereum");
 server.use("/", ethereumRoute);
 
-const priceRoute = require("./routes/PriceUSD")
-server.use("/", priceRoute)
+const priceRoute = require("./routes/PriceUSD");
+server.use("/", priceRoute);
 
+const binanceCode = require("./routes/BinanceSourceCode");
+server.use("/", binanceCode);
 
 server.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
